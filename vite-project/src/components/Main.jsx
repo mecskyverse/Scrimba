bd5e19bdd319a5b1e9be4377d18a52d01e177b3e
Aakash <vermaaakash1000@gmail.com>
2022-07-14 23:35:56 +0530

    Main Page and Navbar Made

---
+++ vite-project/src/components/Main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export default function Main() {
+    return (
+        <h1>Hello its Main Page</h1>
+    )
+}
---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export default function Navbar() {
+    return (
+        <h1>Hello its Navbar Page</h1>
+    )
+}

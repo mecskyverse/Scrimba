72bb682929950333abef03b4db78e56b6be591cd
Aakash <vermaaakash1000@gmail.com>
2022-08-08 22:19:20 +0530

    Info page added

---
+++ vite-project/src/components/Info.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import About from "./About"
+export default function Info() {
+    return (
+        <div>
+
+            <div className="name">Laura Smith</div>
+            <div className="front"> FrontEnd Developer</div>
+            <div className="web"><a href="#" className="web">laurasmith.website</a>
+            </div>
+            <span className="email">Email</span>
+            <span className="linke">Linkedln</span>
+
+        </div>
+    );
+}

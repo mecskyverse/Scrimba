19330e1c31576058bbeab255d2ae4caa44419dcd
Aakash <vermaaakash1000@gmail.com>
2022-08-08 22:20:10 +0530

    Interests section added

---
+++ vite-project/src/components/Interests.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Interest() {
+    return (
+        <div>
+            <div className="about mar-up">
+                <div className="interest">Interest</div>
+                <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+
+            </div>
+        </div>
+    )
+}

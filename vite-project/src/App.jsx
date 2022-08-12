2cb37d0e23de3ca2d3c56c2287962a7cd6a4baed
Aakash <vermaaakash1000@gmail.com>
2022-08-12 21:24:05 +0530

    Directory updated

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
-import Info from "./components/Info.js"
-import About from "./components/About.js"
-import Interest from "./components/Interests.js"
+import Info from "./components/Info.jsx"
+import About from "./components/About.jsx"
+import Interest from "./components/Interests.jsx"
 // import Footer from "./components/Footer.js"
 
 export default function App() {

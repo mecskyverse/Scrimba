0de80f284d3e57ab49aa8180dbbaa0b62771cb33
Aakash <vermaaakash1000@gmail.com>
2022-08-15 23:24:00 +0530

    Footer component initial commit

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,11 +1,17 @@
 import React from "react"
+import Git from "../Images/github.png"
+import Fb from "../Images/fb.png"
+import Twitter from "../Images/twitter.png"
+import Ig from "../Images/ig.png"
+
 
 export default function Footer() {
     return (
         <div className="footer">
-
-            <div className="interest">
-            </div>
+            <img src={Git} alt="" className="icon" />
+            <img src={Twitter} alt="" className="icon" />
+            <img src={Ig} alt="" className="icon" />
+            <img src={Fb} alt="" className="icon" />
 
 
 

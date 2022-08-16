aa3a5bacbcc1b7187f0e12f5f88ec095d85239f4
Aakash <vermaaakash1000@gmail.com>
2022-08-16 19:10:21 +0530

    Links added to footer

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -8,14 +8,11 @@ import Ig from "../Images/ig.png"
 export default function Footer() {
     return (
         <div className="footer">
-            <img src={Git} alt="" className="icon" />
-            <img src={Twitter} alt="" className="icon" />
-            <img src={Ig} alt="" className="icon" />
-            <img src={Fb} alt="" className="icon" />
-
-
-
-        </div>
+            <a href="https://github.com/mecskyverse" target="_blank"><img src={Git} alt="git" className="icon" /></a>
+            <a href="https://twitter.com/mecskyverse" target="_blank"><img src={Twitter} alt="twitter" className="icon" /></a>
+            <a href="https://instagram.com/mecskyverse" target="_blank"><img src={Ig} alt="instagram" className="icon" /></a >
+            <a href="#" target="_blank"><img src={Fb} alt="facebook" className="icon" /></a >
+        </div >
     )
 
 }
